f18e1ef991062c741dce29ea1a0cefbea1d88a38
herince <rherince@gmail.com>
2023-12-13 01:30:18 +0200

    Add example vectors in the J2000 geocentric inertial coordinate system

--- ods/ods.ino
+++ ods/ods.ino
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <QMC5883LCompass.h>
 
 struct t_SunSensor {
@@ -45,10 +46,6 @@ void ReadCompass() {
   compassState.Z /= SENSOR_READS_COUNT;
 }
 
-struct Quaternion {
-  int a, b, c, d;
-};
-
 void setup() {
   // Set data rate
   Serial.begin(9600);
@@ -72,10 +69,33 @@ void loop() {
   // Read and average compass state
   ReadCompass();
 
-  // Calculate first rotation quaternion
+  // Ideally, here I would use actual or realistically simulated satellite-Sun and Earth's magnetic field vectors
+  // in a J2000 geocentric inertial coordinate system. However, I don't have time to figure out
+  // how to this well at the moment so I'm using example normalized values from a random Free Flyer simulation
+  double Sj[3] = {-0.73354462, -0.6235704, -0.270318785};
+  double Bj[3] = {-0.685559737, -0.450165898, -0.572152524};
+
+  // Calculate normal satellite-Sun vector
+  double sunSensorX = double(sensors[0].Value - sensors[3].Value);
+  double sunSensorY = double(sensors[1].Value - sensors[4].Value);
+  double sunSensorZ = double(sensors[2].Value - sensors[5].Value);
+  double Ss[3] = {sunSensorX, sunSensorY, sunSensorZ};
+  double SsMagnitude = sqrt(Ss[0] * Ss[0] + Ss[1] * Ss[1] + Ss[2] * Ss[2]);
+  Ss[0] /= SsMagnitude;
+  Ss[1] /= SsMagnitude;
+  Ss[2] /= SsMagnitude;
+
+  // Calculate normal vector for the Earth's magnetic field with coordinates related to the satellite
+  double Bs[3] = {double(compassState.X), double(compassState.Y), double(compassState.Z)};
+  double BsMagnitude = sqrt(Bs[0] * Bs[0] + Bs[1] * Bs[1] + Bs[2] * Bs[2]);
+  Bs[0] /= BsMagnitude;
+  Bs[1] /= BsMagnitude;
+  Bs[2] /= BsMagnitude;
+
+  // Align Bj and Bs
   // TODO
 
-  // Calculate second rotation quaternion
+  // Align Sj and Ss
   // TODO
   
   delay(250);
